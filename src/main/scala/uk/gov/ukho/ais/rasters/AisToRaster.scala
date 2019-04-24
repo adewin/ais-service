d5c4786107685c89526108c8e0d5c58f8794ef74
Joseph Egan <joseph.egan@ukho.gov.uk>
2019-04-24 15:55:22 +0100

    Add capability to save to S3

--- src/main/scala/uk/gov/ukho/ais/rasters/AisToRaster.scala
+++ src/main/scala/uk/gov/ukho/ais/rasters/AisToRaster.scala
@@ -1,10 +1,15 @@
 package uk.gov.ukho.ais.rasters
 
+import java.io.File
+import java.time.Instant
+
+import com.amazonaws.services.s3.model.PutObjectRequest
 import geotrellis.proj4.CRS
 import geotrellis.raster.io.geotiff.GeoTiff
 import geotrellis.raster.io.geotiff.writer.GeoTiffWriter
 import geotrellis.raster.render.{ColorMap, ColorRamps}
 import geotrellis.raster.{CellSize, IntArrayTile, RasterExtent}
+import geotrellis.spark.io.s3.S3Client
 import geotrellis.vector.Extent
 import org.apache.spark.sql.{Row, SparkSession}
 
@@ -19,10 +24,12 @@ object AisToRaster {
 
   def main(args: Array[String]) {
 
+    val local = false
+
     require(args.length >= 1, "Specify data file")
     val dataFile = args(0)
     require(args.length >= 2, "Specify output directory")
-    val outDir = args(1)
+    val outputLocation = args(1)
     require(args.length == 2, "Too many arguments")
 
     val rasterMatrix = IntArrayTile.fill(0, RASTER_EXTENT.cols, RASTER_EXTENT.rows)
@@ -63,11 +70,22 @@ object AisToRaster {
       }
 
     val geotiff = GeoTiff(rasterMatrix, RASTER_EXTENT.extent, OUTPUT_CRS)
-    GeoTiffWriter.write(geotiff, s"$outDir/test_geotif.tif")
-
     val cm = ColorMap.fromQuantileBreaks(rasterMatrix.histogram, ColorRamps.BlueToRed)
 
-    rasterMatrix.renderPng(cm).write(s"$outDir/test_png.png")
+    val timestamp = Instant.now()
 
+    val tifName = s"raster-$timestamp.tif"
+    val pngName = s"raster-$timestamp.png"
+
+    val localTifPath = if (local) s"$outputLocation/$tifName" else s"/tmp/$tifName"
+    val localPngPath = if (local) s"$outputLocation/$pngName" else s"/tmp/$pngName"
+
+    GeoTiffWriter.write(geotiff, localTifPath)
+    rasterMatrix.renderPng(cm).write(localPngPath)
+
+    if (!local) {
+      S3Client.DEFAULT.putObject(new PutObjectRequest(outputLocation, tifName, new File(localTifPath)))
+      S3Client.DEFAULT.putObject(new PutObjectRequest(outputLocation, pngName, new File(localPngPath)))
+    }
   }
 }
