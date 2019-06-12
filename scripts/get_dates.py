f678bcab3d706052854ddfe82b529274c76e73c2
Ciaran Evans <ciaran.evans@ukho.gov.uk>
2019-06-12 13:57:45 +0100

    Switch to searching for dates straight from .bz2 files (#37)
    * Quicker than downloading the .bz2, unzipping, and traversing file

--- scripts/get_dates.py
+++ scripts/get_dates.py
@@ -1,10 +1,11 @@
 import sys
+import bz2
 
 def determine_start_and_end_dates_for_ais_file(filepath):
   min_date = "9999-99-99 99:99:99"
   max_date = "0000-00-00 00:00:00"
 
-  with open(filepath) as f:
+  with bz2.BZ2File(filepath, "r") as f:
       for line in f:
           datetime = line.split('\t')[2]
           if datetime < min_date:
