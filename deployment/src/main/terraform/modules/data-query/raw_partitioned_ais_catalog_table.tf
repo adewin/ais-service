62de250e9af0909cd0276c569836008f61a61296
Joel Clemence <15887464+JoelClemence@users.noreply.github.com>
2019-07-17 15:33:24 +0100

    Create named query to show start and end dates for input files (#89)

--- deployment/src/main/terraform/modules/data-query/raw_partitioned_ais_catalog_table.tf
+++ deployment/src/main/terraform/modules/data-query/raw_partitioned_ais_catalog_table.tf
@@ -4,3 +4,13 @@ module ais_raw_partitioned_catalog_table {
   store_name    = var.ais_raw_partitioned_store_name
   table_name    = var.ais_raw_catalog_database_table_name
 }
+
+resource aws_athena_named_query original_source_file_start_end_query {
+  database = var.ais_catalog_database_name
+  name     = "original_source_file_start_end_query"
+  query    = <<EOF
+SELECT input_ais_data_file, min(acquisition_time) AS START_DATE, max(acquisition_time) AS END_DATE
+FROM "${var.ais_catalog_database_name}"."${var.ais_raw_catalog_database_table_name}"
+GROUP BY input_ais_data_file;
+EOF
+}
