6b1077cf3fff8856613f60d53b0963fef8c0f06d
Joel Clemence <15887464+JoelClemence@users.noreply.github.com>
2019-08-07 10:23:09 +0100

    Bump jackson databind and add suppressions (#111)

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ allprojects {
     apply { plugin("com.diffplug.gradle.spotless") }
 
     group = "uk.gov.ukho"
-    version = "1.31-SNAPSHOT"
+    version = "1.32-SNAPSHOT"
 
     configure<SpotlessExtension> {
         kotlinGradle {
