03061ba5a3ff6f26075290cc152485ba01563ac1
Joel Clemence <15887464+JoelClemence@users.noreply.github.com>
2019-08-22 16:21:38 +0100

    Add Docker to container (#116)

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ allprojects {
     apply { plugin("com.diffplug.gradle.spotless") }
 
     group = "uk.gov.ukho"
-    version = "1.34-SNAPSHOT"
+    version = "1.35-SNAPSHOT"
 
     configure<SpotlessExtension> {
         kotlinGradle {
