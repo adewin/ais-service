fed287c156686ca908a4b5deb5809e37ee59a740
Joel Clemence <15887464+JoelClemence@users.noreply.github.com>
2019-09-02 09:41:06 +0100

    Supress new vulnerabilities and suppress until October (#127)
    * Bump version of jackson databind
    * Bump version of dependency check library

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ allprojects {
     apply { plugin("com.diffplug.gradle.spotless") }
 
     group = "uk.gov.ukho"
-    version = "1.35-SNAPSHOT"
+    version = "1.36-SNAPSHOT"
 
     configure<SpotlessExtension> {
         kotlinGradle {
