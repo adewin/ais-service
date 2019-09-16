ad5574eb914bb96dd59b4957592587d887ed05a9
George White <georgeunix@gmail.com>
2019-09-16 16:38:52 +0100

    NEWLINES!

--- resampler/src/main/scala/uk/gov/ukho/ais/resampler/model/Ping.scala
+++ resampler/src/main/scala/uk/gov/ukho/ais/resampler/model/Ping.scala
@@ -20,7 +20,6 @@ case class Ping(arkposid: String,
                 flags: String,
                 input_ais_data_file: String) {
   override def toString: String =
-    s"""$arkposid\t$mmsi\t$acquisitionTime\t$longitude\t$latitude\t$vesselClass\t$messageTypeId\t
-       |$navigational_status\t$rot\t$sog\t$cog\t$true_heading\t$altitude\t$special_manoeuvre\t$radio_status\t$flags\t
-       |$input_ais_data_file\n""".stripMargin
+    "$arkposid\t$mmsi\t$acquisitionTime\t$longitude\t$latitude\t$vesselClass\t$messageTypeId\t$navigational_status\t" +
+      "$rot\t$sog\t$cog\t$true_heading\t$altitude\t$special_manoeuvre\t$radio_status\t$flags\t$input_ais_data_file\n"
 }
